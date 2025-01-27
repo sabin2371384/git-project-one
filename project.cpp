10c6ab3ec017745ca54a97bb7e69004ee20fd5b0
Sabina <sabinahosseinpanahi94@gmail.com>
2025-01-27 11:01:30 +0330

    added index.cpp

---
+++ project.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<string> 
+using namespace std;
+\\اطلاعات دانشجو 
+struct student{   
+ string name;
+ string lastname ;
+ string id;
+ string major;
+ vector<course>courses ;
+};
+struct course\\ ساختار درس 
+{
+      string coursenname ;
+      double score ;
+
+};
+student enterstudentinfo()
+{
+
+
+    
+}
