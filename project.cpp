aeaa09f3a4c6cdf9b7003b8665d19dbafb4e1729
Sabina <sabinahosseinpanahi94@gmail.com>
2025-02-01 14:23:58 +0330

    eslahshode

--- project.cpp
+++ project.cpp
@@ -1,58 +1,79 @@
-#include<iostream>
-#include<vector>
-#include<string> 
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
-struct course{
-string coursename ;
-double grade ;
-int units ; 
+
+struct Course {
+    string courseName;
+    double grade;
+    int units;
 };
-void addcourse(string cname, double cgrade, int cunits)
-{  course.push_back({cname,cgrade,cunits});
 
-}
-struct student{   
- string firstname;
- string lastname ;
- string studentID ;
- string major;
- vector<course>courses;
+struct Student {
+    string firstName;
+    string lastName;
+    string studentID;
+    string major;
+    vector<Course> courses ;
 };
-void displayInfo()
-{    cout<<"\nstudent information:\n";
-     cout <<"name ="<<firstname <<"   "<<lastname << endl ;
-     cout <<"major="<< major <<endl ;
-     cout<<"studentID ="<<studentID <<"\n";
+    void addCourse(string cname, double cgrade, int cunits) {
+        courses.push_back({cname, cgrade, cunits});
+    }
 
-}
-int main(){
-     student s;
-     course c;
-     cout<<"Enter first name :";
-     cin>>s.firstname;
-     cout<<"Enter last name:";
-     cin>> s.last name;
-     cout<<"Enter student ID :";
-     cin>> s.studentID ;
-     cout<<"Enter major :";
-     cin>> s.major;
+    // تابع نمایش اطلاعات دانشجو
+    void displayInfo() {
+        cout << "\nStudent Information:\n";
+        cout << "Name: " << firstName << " " << lastName << endl;
+        cout << "Major: " << major << endl;
+        cout << "Student ID: " << studentID << "\n";
+    }
+};
 
-     s.displayInfo();
-     int numcourses ;
-     cout<<"Enter a number of courses=";
-     cin>>numcourses ;
-     for(int i=0;i<numcourses;i++)
-     { string cname ;
-      double cgrade ;
-      int cunits ;
-       cout<<"\nEnter course name :";
-        getline(cin, cname);
-        cout<<"Enter grade :";
-        cin>>cgrade ;
-        cout<<"Enter units :";
-        cin>>cunits ;
-        s.addcourse(cname,cgrade,cunits);
-     }
+int main() {
+    Student s;
 
+    // دریافت اطلاعات دانشجو
+    cout << "Enter first name: ";
+    cin >> s.firstName;
+    cout << "Enter last name: ";
+    cin >> s.lastName;
+    cout << "Enter student ID: ";
+    cin >> s.studentID;
+    cout << "Enter major: ";
+    cin >> s.major;
 
+    s.displayInfo();
+
+    int numCourses;
+    cout << "Enter number of courses: ";
+    cin >> numCourses;
+    
+    cin.ignore();
+
+    for (int i = 0; i < numCourses; i++) {
+        string cname;
+        double cgrade;
+        int cunits;
+
+        cout << "\nEnter course name: ";
+        getline(cin, cname); 
+
+        cout << "Enter grade: ";
+        cin >> cgrade;
+
+        cout << "Enter units: ";
+        cin >> cunits;
+        cin.ignore();
+
+        s.addCourse(cname, cgrade, cunits); 
+    }
+
+    // نمایش اطلاعات دروس دانشجو
+    cout << "\nStudent Courses:\n";
+    for (const auto &course : s.courses) {
+        cout << "Course Name: " << course.courseName << ", Grade: " << course.grade << ", Units: " << course.units << endl;
+    }
+
+    return 0;
 }
